9b8b96b38829db0b594b10783515e37eb1b87ac3
KonstantinKok <konstantin.nikolaevish.14.11@gmail.com>
2024-11-17 11:15:50 +0300

    17.11
    Для сдачи

--- UrbanDjango/task3/views.py
+++ UrbanDjango/task3/views.py
@@ -3,7 +3,7 @@ from lib2to3.fixes.fix_input import context
 from django.template.context_processors import request
 from django.views.generic import TemplateView
 
-def index_home(request):
+def home(request):
     title= 'Cайт Django'
     text= 'Главная страница'
     context = {
@@ -13,13 +13,22 @@ def index_home(request):
     return render(request, 'third_task/home.html', context)
 
 
-def home(request):
-    return render(request, 'third_task/home.html')
-
 def shop(request):
-    return render(request, 'third_task/shop.html')
+    title= 'Магазин'
+    text= 'Магазин игр'
+    context = {
+        'title': title,
+        'text': text,
+    }
+    return render(request, 'third_task/shop.html', context)
 
 def cart(request):
-    return render(request, 'third_task/cart.html')
+    title = 'Корзина'
+    text = 'Извините корзина пуста'
+    context = {
+        'title': title,
+        'text': text,
+    }
+    return render(request, 'third_task/cart.html', context)
 
 # Create your views here.
