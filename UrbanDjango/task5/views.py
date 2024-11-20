9e4693ec7fc4ec9c2902f5b16e162c6b29f7c936
KonstantinKok <konstantin.nikolaevish.14.11@gmail.com>
2024-11-20 15:40:16 +0300

    20.11

--- UrbanDjango/task5/views.py
+++ UrbanDjango/task5/views.py
@@ -10,21 +10,18 @@ def register(request):
         repeat_password = request.POST.get('repeat_password')
         age = request.POST.get('age')
 
-        if username in users:
-            return HttpResponse(f'Имя {username} - занято. Выберите другое имя')
-        else:
-            users.append(username)
+        if username in users and password == repeat_password and int(age) > 18:
+            return HttpResponse(f'Приветствуем, {username}!')
 
-        if password != repeat_password:
-            return HttpResponse(f'{username}, Пароли не совпадают!!!')
+        elif username in users:
+             return HttpResponse(f'Имя {username} - занято. Выберите другое имя')
+
+        elif password != repeat_password:
+             return HttpResponse(f'{username}, Пароли не совпадают!!!')
+
         elif int(age) < 18:
-            return HttpResponse(f'{username}, Вам нет 18!!!')
-        elif password == repeat_password and int(age) > 18:
-            return HttpResponse(f'{username}, успешно зарегистрировались!!!')
+             return HttpResponse(f'{username}, Вам нет 18!!!')
 
-    info = {'username': 'username', 'password': 'password', 'repeat_password': '<PASSWORD>', 'age': 'age'}
-    return render(request, "task5/registration_page.html", context=info)
-
-
+    return render(request, "task5/registration_page.html")
 
 # Create your views here.
